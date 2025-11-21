0253b004434672ce1fe591367af64d68378fb877
MarcosPauloG7 <alves.paulo1@academico.ifrn.edu.br>
2025-11-20 21:01:00 -0300

    Grandes mudanças, documentação e licença

--- LeitorCSV.py
+++ LeitorCSV.py
@@ -6,19 +6,7 @@ class LeitorCSV:
         self.csv_file = csv_file
 
     def ler(self):
-        # with open(self.csv_file, mode='r', encoding='utf-8') as csvfile:
-        #     tabela = csv.reader(csvfile, delimiter=',')
-        #     for linha in tabela:
-        #         cpf = linha[0]
-        #         self.lista_cpfs.append(cpf)
-        # return self.lista_cpfs
-
         df = pd.read_csv(self.csv_file)
-        lista_cpfs = df['cpfs'].tolist()
-
+        lista_cpfs = df['cpfs'].astype(str).tolist()
+        lista_cpfs = sorted(set(lista_cpfs))
         return lista_cpfs
-
-
-if __name__ == "__main__":
-    leitor = LeitorCSV("cpfs.csv")
-    print(leitor.ler())
